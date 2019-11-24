1e51ee33fe6ce78d8412606b29530a18fa1a74b2
hanbaoyi <604594231@qq.com>
2019-11-24 11:18:05 +0800

    words_of_string.rb

---
+++ Ruby面向对象/words_of_string.rb
@@ -0,0 +1,15 @@
+class String
+  def to_words
+    self.gsub(/[^\da-zA-Z\s]/, '') # 注意 self. 可以省略
+  end
+end
+
+require 'test/unit'
+
+class ToWordsTest < Test::Unit::TestCase
+  def test_1
+    assert_equal '3 the Magic Number', '#3, the *Magic, Number*?'.to_words
+  end
+end
+
+# 接下来请查看 about_class.rb
